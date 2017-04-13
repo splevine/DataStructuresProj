1c8559c15067acea0107f7a9bddc1b5b4a67983b
SL <levine.seth.p@gmail.com>
2017-04-13 13:56:55 -0400

    Update Skeleton
    Added Restaurant Class integration
    Added Restaurant sample

--- Skeleton.java
+++ Skeleton.java
@@ -1,5 +1,5 @@
 package skeleton;
-/** April 6th, 2017
+/** April 13th, 2017
  * Data Structures Project - rough draft
  * Prof. Nizich
  * Inclusion of Data Structures through Queues
@@ -16,20 +16,35 @@ import java.util.*;
 // Binary Tree, Binary Search, Bubble Sort
 // We will probably want to create some other classes
 
-public class Skeleton {
+public class Skeleton1 {
     public static void main(String[] args) {
-        // This is just dummy code for now
+        // Skeleton Code for now
+        
         // Array of Strings
-        String[] myArray = { "Restaurant 1", "Restaurant 2", "Holder 3" };
-        System.out.println("myArray: " + myArray);
-        System.out.println("Any ideas why it's printing that?");
-        //myArray is printing a memory address on my system for some reason
+        String[] myArray = { "Restaurant 1", "Restaurant 2", "Restaurant 3" };
+        System.out.println("myArray: ");
+        for (int i=0; i < myArray.length; i++){ //arrays use array.length for 'size'
+        System.out.println(myArray[i] + " "); //array[index] to access array elements
+        }
         
         // Arraylist - this isn't on requirements, but we can use it if we want
         ArrayList obj = new ArrayList();
         obj.add("Holder 1");
         obj.add("Holder 2");
         System.out.println("ArrayList: " + obj);
+        
+        ArrayList<Integer> vals = new ArrayList<>();
+        vals.add(10);
+        vals.add(15);
+        vals.add(20);
+        vals.add(25);
+        System.out.println("Vals: " + vals);
+        int sum = 0;
+        for (int i = 0; i < vals.size(); i++){ //arrayList use obj.size() for 'length'
+            sum = sum + vals.get(i); //arrayList use obj.get to access elements
+        }    
+        
+        System.out.println("Sum of Vals: " + sum);
         
         // LinkedList
         LinkedList<String> myLinkedList = new LinkedList<>();
@@ -46,15 +61,32 @@ public class Skeleton {
        
         // Queue
         Queue<String> myQ = new LinkedList<String>();
-        myQ.add("Queue 1");
-        myQ.add("Queue 2");
-        myQ.add("Queue 3");
+        myQ.add("Monday");
+        myQ.add("Tuesday");
+        myQ.add("Wednesday");
+        myQ.add("Thursday");
+        myQ.add("Friday");
         System.out.println("myQ: " + myQ);
         
+        //
+        //TreeSet<String> ts = new TreeSet<String>();
+        
+        //ts.add("C");
+        //ts.add("A");
+        //ts.add("B");
+        //ts.add("E");
+        //ts.add("F");
+        //ts.add("D");
+        
+        //System.out.println("myTree: " + ts);
+
         //binary tree
         //binary search
         //bubble sort
        
+        Restaurant Pizza = new Restaurant("Pizza", "20 Any Street", "Italian", 50);
+        System.out.println (Pizza.getFullInfo());
+
         
     }
     
