234edfd0b0de020e93ce9699393e8717c5212c37
SL <levine.seth.p@gmail.com>
2017-04-18 22:54:08 -0400

    Added inputWaste() Method +
    added Array for each day of week waste
    added int var for each day of the week
    ** inputWaste() method ** - uses scanner to take in ints
    createArray() for waste array

--- Restaurant.java
+++ Restaurant.java
@@ -1,19 +1,31 @@
-package skeleton1;
+
+package datastructuresproj;
+
+import java.util.Scanner;
+
 /**
  *
  * @author Seth
  */
 public class Restaurant {
+    Scanner sc = new Scanner(System.in);
+
     private String name;
     private String address;
     private String foodType;
     private int foodWaste;
+    private int MonWaste;
+    private int TuesWaste;
+    private int WedsWaste;
+    private int ThurWaste;
+    private int FriWaste;    
+    int[] waste = new int[5];    
     
     public Restaurant(String name){
         this.name = name;
     }
     
-    public Restaurant(String name, String address, String foodType, int foodWaste){
+    public Restaurant(String name, String address, String foodType){
         this.name = name;
         this.address = address;
         this.foodType = foodType;
@@ -48,4 +60,38 @@ public class Restaurant {
                 + "\nFood Type: " + foodType + "\nWasted Food: **" + foodWaste + "** lbs";
         return printInfo;
     }
+    
+    public int[] createArray() {
+        int[] waste = new int[5];
+        waste[0] = MonWaste; 
+        waste[1] = TuesWaste; 
+        waste[2] = WedsWaste; 
+        waste[3] = ThurWaste;
+        waste[4] = FriWaste;
+        return waste;
+    }
+    
+    public void inputWaste(){
+    System.out.println("Please Enter the amount of food wasted each day at " + this.getName());
+        for (int i = 0; i < 5; i++){
+            if (i==0) {
+                System.out.println("Monday Waste: ");
+            }
+            if (i==1) {
+                System.out.println("Tuesday Waste: ");
+            }
+            if (i==2) {
+                System.out.println("Wednesday Waste: ");
+            }
+            if (i==3) {
+                System.out.println("Thursday Waste: ");
+            }       
+            if (i==4) {
+                System.out.println("Friday Waste: ");
+            }                   
+            this.waste[i] = sc.nextInt();
+        }
+    }
+    
 }
+
