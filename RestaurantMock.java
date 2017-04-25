4a83da4d4cc8b816b4ed8a1ee0e18ccf2442dd88
SL <levine.seth.p@gmail.com>
2017-04-25 16:34:10 -0400

    Create RestaurantMock.java

---
+++ RestaurantMock.java
@@ -0,0 +1,16 @@
+package datastructuresproj;
+
+/**
+ *
+ * @author Seth
+ */
+public class RestaurantMock {
+    //Constructor for Restaurant(String name, String address, Double distance, String manager){
+
+    Restaurant App = new Restaurant("App-itizer", "6 Engineers Rd, Roslyn, NY 11576", 2.4, "John Alonzo");
+    Restaurant Franks = new Restaurant("Frank's Pizza & Pasta", "2 Whippet Ln, East Norwich, NY 11732", 6.9, "Cody Binks");
+    Restaurant Pasta = new Restaurant("Pastafrian Extravaganza", "8 Feeks Ln, Locust Valley, NY 11560", 6.8, "Shani Jones Jackson III");
+    Restaurant Mex = new Restaurant ("The Whole Enchilada", "35 Greenmeadow Ln, Huntington, NY 11743", 5.6, "Gabriel Rayes");
+    Restaurant Buffet = new Restaurant ("The Grand Buffet", "90 Crestwood Blvd, Farmingdale, NY 11735", 8.1, "James Chong");
+
+}
